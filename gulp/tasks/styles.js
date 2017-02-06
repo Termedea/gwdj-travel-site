034baac507fd7ddeedfe415d7c768a2895c36096
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-02-06 17:59:28 +0100

    repo broke again, recommit

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+var gulp 			= require("gulp"),
+	postCSS 		= require("gulp-postcss"),
+	autoprefixer 	= require("autoprefixer"),
+	cssVars			= require("postcss-simple-vars"),
+	cssNested		= require("postcss-nested"),
+	cssImport		= require("postcss-import"),
+	cssMixins		= require("postcss-mixins");
+
+
+gulp.task("styles", function(){
+	//convert dev css-structure to styles.css via PostCss-plugins
+	return gulp.src("./app/assets/styles/styles.css")
+				.pipe(postCSS([cssImport, cssMixins, cssVars, cssNested, autoprefixer]))
+				.on("error", function(err){
+					console.log("An error occured \n" +err.toString());
+					this.emit("end");
+				})
+				.pipe(gulp.dest("./app/tmp/styles"));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp 			= require("gulp"),
+	watch 			= require("gulp-watch"),
+	browserSync		= require("browser-sync").create();
+
+
+gulp.task("watch", function(){
+	
+	browserSync.init({
+		server:{
+			baseDir: "app"
+		},
+		notify: false
+	});
+
+	watch("./app/index.html", function(){
+		browserSync.reload();
+	}); //Two arguments 1. file to monitor for changes, 2. function with what we want to do, i.e. run a task. 
+
+	watch("./app/assets/styles/**/*.css", function(){
+		gulp.start("cssInject");
+
+	}); //the directory notation '**' matches 0 or many directories, which makes it watch any future folders, if any.
+});
+
+gulp.task("cssInject", ['styles'], function(){ //styles is a dependecy of the cssInject task. The cssInject task will not start before styles is complete. 
+	return gulp.src("./app/tmp/styles/styles.css")
+			.pipe(browserSync.stream());
+
+});
