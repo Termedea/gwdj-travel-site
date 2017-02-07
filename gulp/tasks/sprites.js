45df9c1c2277914dd6608eb15a1c01bbaf5df8f2
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-02-07 14:30:22 +0100

    Automatic sprite generation and change in html to use new sprite

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,48 @@
+var gulp 		= 	require("gulp"),
+	svgSprite 	=	require("gulp-svg-sprite"),
+	rename 		= 	require("gulp-rename"), 
+	del 		= 	require("del");
+
+
+var spriteConfig =  {
+	mode: {
+		css: {
+			sprite: "sprite.svg",
+			render: {
+				css: {
+					template: './gulp/templates/sprite.css'
+				}
+			}
+		}
+	}
+}
+
+gulp.task("beginClean", function(){
+	return del(["./app/tmp/sprite", "./app/assets/images/sprites"]);
+
+});
+
+gulp.task("createSprite", ["beginClean"], function(){
+
+	return gulp.src("./app/assets/images/icons/**/*") /*everything, including current and future subfolders within the icons folder.*/
+		.pipe(svgSprite(spriteConfig))
+		.pipe(gulp.dest("./app/tmp/sprite/"));
+
+});
+
+gulp.task("copySpriteGraphic", ["createSprite"], function(){
+	return gulp.src("./app/tmp/sprite/css/**/*.svg")
+		.pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+gulp.task("copySpriteCSS", ["createSprite"], function(){
+	return gulp.src("./app/tmp/sprite/css/*.css")
+		.pipe(rename("_sprite.css"))
+		.pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("endClean", ["copySpriteGraphic", "copySpriteCSS"], function(){
+	return del(["./app/tmp/sprite"]);
+});
+
+gulp.task("icons", ["beginClean", "createSprite", "copySpriteCSS", "copySpriteGraphic", "endClean"]);
