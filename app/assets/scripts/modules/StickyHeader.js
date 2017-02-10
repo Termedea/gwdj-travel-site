d1fa41a493f014305765aabbf6b17722bad64ff9
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2017-02-10 13:28:30 +0100

    sticky header with smooth scroll

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,28 +1,59 @@
 import $ from "jquery";
 import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints"; //.modules/.. (scripts)/.. (assets)/.. (app)/.. (travel-site)
+import smoothScroll from "jquery-smooth-scroll";
 
 class StickyHeader{
 
 	constructor(){
 		this.siteHeader = $(".site-header");
 		this.headerTriggerElement = $(".large-hero__title");
+		this.pageSections = $(".page-section");
+		this.headerLinks = $(".primary-nav a");
 		this.createHeaderWaypoint();
+		this.createPageSectionWaypoints("down", "18%");
+		this.createPageSectionWaypoints("up", "-40%");
+		this.addSmoothScrolling();
+	}
+
+	addSmoothScrolling(){
+		this.headerLinks.smoothScroll();
 	}
 
 	createHeaderWaypoint(){
 		
-		var that = this; 
+		var obj = this; 
 		new Waypoint({
-			element: that.headerTriggerElement[0],
+			element: obj.headerTriggerElement[0],
 			handler: function(direction){
 				if(direction == "down"){
-					that.siteHeader.addClass("site-header--dark");
+					obj.siteHeader.addClass("site-header--dark");
 				}else{
-					that.siteHeader.removeClass("site-header--dark");
+					obj.headerLinks.removeClass("is-current-link");
+					obj.siteHeader.removeClass("site-header--dark");
 				}
 			}
 		});
 	}
-}
 
+	createPageSectionWaypoints(dir, offset){
+
+		var obj = this;
+
+		this.pageSections.each(function(){
+			var pageSection = this;
+			new Waypoint({
+				element: pageSection,
+				handler: function(direction){
+
+					if(direction == dir){
+						var matchingHeaderLink = pageSection.getAttribute("data-link");
+						obj.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");						
+					}
+				},
+				offset: offset
+			});
+		});		
+	}
+}
 export default StickyHeader
